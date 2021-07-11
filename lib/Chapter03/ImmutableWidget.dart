f75978cc0d6e34bfa9406debd6115c841ad7c117
maxgdev <max@maxgdev.net>
2021-07-11 19:17:24 +0100

    Added Transform.rotate() and BoxDecoration to ImmutableWidget

--- lib/Chapter03/ImmutableWidget.dart
+++ lib/Chapter03/ImmutableWidget.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class ImmutableWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: Padding(
-        padding: EdgeInsets.all(40),
-        child: Container(
-          color: Colors.purple,
-          child: Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: _buildShinyCircle(),
+      child: Center(
+        child: Transform.rotate(
+          angle: 180 / Math.pi, // Rotations in radians
+          child: Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.deepPurple.withAlpha(120),
+                  spreadRadius: 4,
+                  blurRadius: 15,
+                  offset: Offset.fromDirection(1.0, 30),
+                ),
+              ],
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: _buildShinyCircle(),
 
-            // child: Container(
-            //   color: Colors.blue,
-            // ),
+              // child: Container(
+              //   color: Colors.blue,
+              // ),
+            ),
           ),
         ),
       ),
