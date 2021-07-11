2386b739679919529a696d19b0d78657646c5ed6
maxgdev <max@maxgdev.net>
2021-07-11 19:26:22 +0100

    Added Scaffold, AppBar and styling properties to Chapter03 widget

--- lib/Chapter03/Chapter03Page.dart
+++ lib/Chapter03/Chapter03Page.dart
@@ -5,8 +5,17 @@ import 'package:packt_cookbook/Chapter03/BasicScreen.dart';
 class Chapter03Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BasicScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("Flutter Cookbook: Chapter 03"),
+        actions: [
+          Padding(padding: const EdgeInsets.all(10.0),
+          child: Icon(Icons.edit),
+          )
+        ],
+      ),
+      body: BasicScreen(),
     );
   }
 }
