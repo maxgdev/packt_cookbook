065d2b35106566431848df6856f9dbdb2680743f
maxgdev <max@maxgdev.net>
2021-07-12 22:44:33 +0100

    Added Text widgets and styling to TextLayout

--- lib/Chapter03/TextLayout.dart
+++ lib/Chapter03/TextLayout.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 class TextLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 16),
+        ),
+        Text(
+          'Text can wrap without issue',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        
+        Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscingcelit. Etiam at mauris massa. Suspendisse potenti.cAenean aliquet eu nisl vitae tempus.'),
+      ],
+    );
   }
 }
