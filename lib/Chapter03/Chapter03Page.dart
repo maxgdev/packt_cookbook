86f1c95ad9f642beeb1c677981c3e0d448885cb4
maxgdev <max@maxgdev.net>
2021-07-12 22:37:14 +0100

    Added TextLayout class

--- lib/Chapter03/Chapter03Page.dart
+++ lib/Chapter03/Chapter03Page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:packt_cookbook/Chapter03/BasicScreen.dart';
+import 'package:packt_cookbook/Chapter03/ImmutableWidget.dart';
+import 'package:packt_cookbook/Chapter03/TextLayout.dart';
 // import '../Chapter03/ImmutableWidget.dart';
 
 class Chapter03Page extends StatelessWidget {
@@ -10,18 +12,28 @@ class Chapter03Page extends StatelessWidget {
         backgroundColor: Colors.indigo,
         title: Text("Flutter Cookbook: Chapter 03"),
         actions: [
-          Padding(padding: const EdgeInsets.all(10.0),
-          child: Icon(Icons.edit),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(Icons.edit),
           )
         ],
       ),
-      body: BasicScreen(),
-      drawer: Drawer(child: Container(
-        color: Colors.lightBlue,
-        child: Center(
-          child: Text("I'm a Flutter Drawer!"),
+      // body: BasicScreen(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(aspectRatio: 1.0, child: ImmutableWidget()),
+          TextLayout(),
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text("I'm a Flutter Drawer!"),
+          ),
         ),
-      ),),
+      ),
     );
   }
 }
