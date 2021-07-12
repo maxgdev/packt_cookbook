9d58e69c80a23cd54dbf14a365c736d9112be6bf
maxgdev <max@maxgdev.net>
2021-07-12 22:48:56 +0100

    Added RichText widget to TextLayout

--- lib/Chapter03/TextLayout.dart
+++ lib/Chapter03/TextLayout.dart
@@ -8,15 +8,40 @@ class TextLayout extends StatelessWidget {
       children: <Widget>[
         Text(
           'Hello, World!',
-          style: TextStyle(fontSize: 16),
+          style: TextStyle(fontSize: 46),
         ),
         Text(
           'Text can wrap without issue',
           style: Theme.of(context).textTheme.headline6,
         ),
-        
         Text(
             'Lorem ipsum dolor sit amet, consectetur adipiscingcelit. Etiam at mauris massa. Suspendisse potenti.cAenean aliquet eu nisl vitae tempus.'),
+        Divider(),
+        RichText(
+          text: TextSpan(
+            text: 'Flutter text is ',
+            style: TextStyle(fontSize: 22, color: Colors.black),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'really ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'powerful.',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.double,
+                      fontSize: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
