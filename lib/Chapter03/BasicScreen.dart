73348244a6ed5cdbcbdd4223051fc627cafb9052
maxgdev <max@maxgdev.net>
2021-07-14 23:32:16 +0100

    Added images folder, image and image.asset

--- lib/Chapter03/BasicScreen.dart
+++ lib/Chapter03/BasicScreen.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:packt_cookbook/Chapter03/ImmutableWidget.dart';
+import './TextLayout.dart';
 
 class BasicScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 1.0,
-          child: ImmutableWidget(),
-        ),
+        body: Center(
+      //   child: AspectRatio(
+      //     aspectRatio: 1.0,
+      //     child: ImmutableWidget(),
+      //   ),
+      // ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.asset('images/oladimeji-odunsi-wecunh3nBrA-unsplash.jpg'),
+          TextLayout(),
+        ],
       ),
-    );
+    ));
   }
 }
