e50e5ee6e18e8629f87b73eb0c1d336221557a84
maxgdev <max@maxgdev.net>
2021-07-16 21:41:22 +0100

    Added _buildProfileImagge() to ProfileScreen()

--- lib/Chapter04/ProfileScreen.dart
+++ lib/Chapter04/ProfileScreen.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+
 class ProfileScreen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return Container();
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _buildProfileImage(context),
+          _buildProfileDetails(context),
+          _buildActions(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileImage(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      child: ClipOval(
+        child: Image.asset(
+          'images/ash-edmonds-black-panther-unsplash.jpg',
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileDetails(BuildContext context) {
+    return Container();
+  }
+
+  Widget _buildActions(BuildContext context) {
+    return Container();
+  }
 }
